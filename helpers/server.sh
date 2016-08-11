159387500d0f61ce1e68339d1dcca912e5d02baf
Luiz Felipe F M Costa <phelip@thenets.org>
2016-08-11 03:24:01 -0300

    Add run server for development mode on command line

--- helpers/server.sh
+++ helpers/server.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-sudo su -s /bin/bash - ckan -c ". /usr/lib/ckan/default/bin/activate && paster serve --reload /etc/ckan/default/development.ini"
+su -s /bin/bash - ckan -c ". /usr/lib/ckan/default/bin/activate && paster serve --reload /etc/ckan/default/development.ini"
