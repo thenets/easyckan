5d72f0f2f869a10e25ca21f31563140be31378fc
Luiz Felipe F M Costa <phelip@thenets.org>
2016-08-19 13:53:12 -0300

    DataPusher add to command line

--- helpers/datapusher.sh
+++ helpers/datapusher.sh
@@ -4,7 +4,7 @@
 DATASETS="/tmp/ckan_datasets"
 rm -f $DATASETS
 touch $DATASETS
-/etc/easyckan/bin/easyckan paster dataset list > $DATASETS
+timeout 30s su -c "/etc/easyckan/bin/easyckan paster dataset list > $DATASET"
 
 # Remove useless information lines
 tail -n +3 "$DATASETS" > "$DATASETS.tmp"
@@ -17,6 +17,6 @@ cat /tmp/ckan_datasets | while read line ; do
   echo ""
   echo "# Update Dataset: $line"
   echo "| ================================================"
-  timeout 30m su -c "echo -ne 'y\n' | /etc/easyckan/bin/easyckan paster datapusher submit $line"
+  timeout 3m su -c "echo -ne 'y\n' | /etc/easyckan/bin/easyckan paster datapusher submit $line"
   sleep 2
 done
