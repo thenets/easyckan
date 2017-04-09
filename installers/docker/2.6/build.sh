84482f493ebf11c1c09da3f300c39de908198e70
Luiz Felipe F M Costa <phelip@thenets.org>
2017-04-09 03:58:47 -0300

    Set correct tags to Dockerfiles

---
+++ installers/docker/2.6/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Version
+CKAN_VERSION="2.6"
+
+# All Docker CKAN images
+CKAN_IMAGES=(ckan ckan-cli ckan-dev ckan-postgres ckan-production ckan-solr)
+
+# Build each image
+for i in ${CKAN_IMAGES[@]}; do
+    echo "# Building easyckan/"${i}":"$CKAN_VERSION
+    echo "# ====================================================="
+    docker build -f installers/docker/$CKAN_VERSION/${i}/Dockerfile \
+                -t easyckan/${i}:$CKAN_VERSION \
+                installers/docker/$CKAN_VERSION/${i}
+    echo ""
+    echo ""
+done
