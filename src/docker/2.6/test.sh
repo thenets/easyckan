c3b497ef7e897776c4d0652f2b0fabd186034ef5
Luiz Costa <phelip@thenets.org>
2017-11-12 05:48:17 -0200

    Travis CI test

---
+++ src/docker/2.6/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Dependencies
+# ===========================================================
+echo "Starting EasyCKAN dependencies..."
+easyckan exec pwd
+
+echo "Installing Mocha for front-end tests..."
+npm install -g mocha-phantomjs@3.5.0 phantomjs@~1.9.1
+
+
+# Test Front-end
+# ===========================================================
+
+# Start CKAN server
+easyckan dev &
+sleep 5 # Make sure the server has fully started
+
+# Run test
+mocha-phantomjs http://localhost:5000/base/test/index.html
+
+# Did an error occur?
+MOCHA_ERROR=$?
+
+# Did an error occur?
+[ "0" -ne "$MOCHA_ERROR" ] && echo MOCHA tests have failed
+
+
+# Remove all containers
+# ===========================================================
+docker rm -f $(docker ps -qa)
+
+# Error output to Travis
+# ===========================================================
+exit `expr $MOCHA_ERROR + $NOSE_ERROR`
